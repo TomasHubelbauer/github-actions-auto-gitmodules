aa50974a2e61749fe4ba3fa3f5360dd82c137c0d
Tomas Hubelbauer <tomas@hubelbauer.net>
2023-05-06 16:36:01 +0200

    Improve runCommand error reporting and allow swapping streams
    For some commands we expect only the standard error and for other only the standard output and this toggle allows checking for either case.

--- runCommand.js
+++ runCommand.js
@@ -3,15 +3,31 @@ import util from 'util';
 
 const exec = util.promisify(child_process.exec);
 
-export default async function runCommand(command) {
-  const { stdout, stderr, exitCode } = await exec(command);
-  if (stderr) {
-    throw new Error(stderr);
+/**
+ * 
+ * @param {string} command The command to execute
+ * @param {*} swapStd Whether to treat stderr (true) or stdout (false) as failure
+ * @returns 
+ */
+export default async function runCommand(command, swapStd = false) {
+  /** @type {string} */
+  let stdout;
+
+  /** @type {string} */
+  let stderr;
+
+  try {
+    const streams = await exec(command);
+    stdout = streams.stdout;
+    stderr = streams.stderr;
+  }
+  catch ({ code, killed, signal, stdout, stderr }) {
+    throw new Error(`'${command}' exited with a non-zero exit code ${code}${killed ? ` due to being killed by ${signal}` : ''}.\n\nStandard output:\n${stdout}\n\nStandard error:\n${stderr}`);
   }
 
-  if (exitCode !== undefined && exitCode !== 0) {
-    throw new Error(`Exit code ${exitCode} from command: ${command}: ${stdout}`);
+  if (swapStd ? stdout : stderr) {
+    throw new Error(`'${command}' exited with code zero with non-empty standard ${swapStd ? 'output' : 'error'}.\n\nStandard output:\n${stdout}\n\nStandard error:\n${stderr}`);
   }
 
-  return stdout;
+  return swapStd ? stderr : stdout;
 }
